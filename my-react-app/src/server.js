58be98e8313ae2080c985db5aeb61c336d3ba051
Ruthesh-Thavamani <truthesh@gmail.com>
2024-04-13 23:31:35 -0400

    modified server.js

--- my-react-app/src/server.js
+++ my-react-app/src/server.js
@@ -2,19 +2,23 @@ const app = require('express');
 const spawn = require("child_process").spawn;
 const port = 3000
 
+// Require the upload middleware
+const upload = require('./upload');
+
 app.post('/upload', upload.single('file'), (req, res) => {
     console.log("upload button clicked.... i think")
     res.setHeader('Access-Control-Allow-Origin', '*');
-    try {
-        var store = req.query.store;
-        var price = req.query.price;
+    if (!req.file) {
+        return res.status(400).send('No files were uploaded.');
     }
-    catch {
-        console.log("failed to render API")
-        res.send("ERROR 403: FAILED: INCORRECT PARAMS: '/retrieve?store=...&price=....");
-    }
+    res.send('File uploaded successfully.');
+
     spawn('python3',['handle_card.py', store, price.toString()]).stdout.on('data', (data) => {
         console.log("python: " + data);
         res.send(data);
     });
+});
+
+app.listen(port, () => {
+    console.log("listening...");
 });
